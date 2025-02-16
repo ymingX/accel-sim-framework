77e3d9a27abf627ba4d63e7d2d4d88decaafd742
ymx <yuming.x@outlook.com>
2025-02-16 17:29:00 +0800

    Downgrade NVbit v1.7.3 to v1.7.0; update init.sh

--- init.sh
+++ init.sh
@@ -14,13 +14,14 @@ echo "==========Init gpu-app-collection: Start========"
 {
         echo "==========Init gpu-app-collection: Start========"
         # Get the applications, their data files and build them:
-        git clone git@github.com:accel-sim/gpu-app-collection.git
-        # source ./gpu-app-collection/src/setup_environment
-        # make -j -C ./gpu-app-collection/src rodinia_2.0-ft
+        # git clone git@github.com:accel-sim/gpu-app-collection.git
+        git clone git@github.com:ymingX/gpu-app-collection.git
+        source ./gpu-app-collection/src/setup_environment
+        # make -j -C ./gpu-app-collection/src all
         # make -C ./gpu-app-collection/src data
 
         # Run the applications with the tracer (remember you need a real GPU for this):
-        # ./util/tracer_nvbit/run_hw_trace.py -B rodinia_2.0-ft -D <gpu-device-num-to-run-on>
+        # ./util/tracer_nvbit/run_hw_trace.py -B GPU_Microbenchmark -D <gpu-device-num-to-run-on>
 
         echo "==========Init gpu-app-collection: Finished====="
 } >  gpu-app-collection.log 2>&1
@@ -32,7 +33,7 @@ echo "==========Init simulator: Start========"
         echo "==========Init simulator: Start========"
         pip install -r requirements.txt
         source ./gpu-simulator/setup_environment.sh
-        make -j 80 -C ./gpu-simulator/
+        make -j  -C ./gpu-simulator/
         echo "==========Init simulator: Finished====="
 } >  simulator.log 2>&1
 echo "==========Init simulator: Finished====="
