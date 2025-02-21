baf73a9bda7fa02974e8c687f083c60278c24735
ymx <yuming.x@outlook.com>
2025-02-21 13:02:03 +0800

    new a dummy Ada ISA in gpu-simulator/ISA_Def/dummy_ada_opcode.h, copied from ampere_opcode.h. And register it in gpu-simulator/trace-driven/trace_driven.cc line 105

--- gpu-simulator/trace-driven/trace_driven.cc
+++ gpu-simulator/trace-driven/trace_driven.cc
@@ -42,6 +42,7 @@
 
 #include "../ISA_Def/accelwattch_component_mapping.h"
 #include "../ISA_Def/dummy_hopper_opcode.h"
+#include "../ISA_Def/dummy_ada_opcode.h"
 #include "../ISA_Def/ampere_opcode.h"
 #include "../ISA_Def/kepler_opcode.h"
 #include "../ISA_Def/pascal_opcode.h"
@@ -103,11 +104,13 @@ trace_kernel_info_t::trace_kernel_info_t(dim3 gridDim, dim3 blockDim,
   m_was_launched = false;
 
   // resolve the binary version
-  if (kernel_trace_info->binary_verion == AMPERE_RTX_BINART_VERSION ||
-      kernel_trace_info->binary_verion == AMPERE_A100_BINART_VERSION)
-    OpcodeMap = &Ampere_OpcodeMap;
-  else if (kernel_trace_info->binary_verion == HOPPER_H100_BINART_VERSION)
+  if (kernel_trace_info->binary_verion == HOPPER_H100_BINART_VERSION)
     OpcodeMap = &Hopper_OpcodeMap;
+  else if (kernel_trace_info->binary_verion == ADA_BINART_VERSION)
+    OpcodeMap = &Ada_OpcodeMap;
+  else if (kernel_trace_info->binary_verion == AMPERE_RTX_BINART_VERSION ||
+           kernel_trace_info->binary_verion == AMPERE_A100_BINART_VERSION)
+    OpcodeMap = &Ampere_OpcodeMap;
   else if (kernel_trace_info->binary_verion == VOLTA_BINART_VERSION)
     OpcodeMap = &Volta_OpcodeMap;
   else if (kernel_trace_info->binary_verion == PASCAL_TITANX_BINART_VERSION ||
